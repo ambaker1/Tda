a9ef501cb14cda88c764c6a284c9702431fe5b62
Alex Baker <ambaker1@mtu.edu>
2023-03-26 12:18:02 -0500

    Cleaned up to match documentation

--- tests/vis_test.tcl
+++ tests/vis_test.tcl
@@ -6,14 +6,14 @@ set y1 [vmap sin $x]
 set y2 [vmap cos $x]
 plotXY $x $y1 $y2
 # Table module
-set tableObj [tdatbl new]
-$tableObj define data {
+set tblObj [tbl new]
+$tblObj define data {
     1 {x 3.44 y 7.11 z 8.67}
     2 {x 4.61 y 1.81 z 7.63}
     3 {x 8.25 y 7.56 z 3.84}
     4 {x 5.20 y 6.78 z 1.11}
     5 {x 3.26 y 9.92 z 4.56}
 }
-viewTable $tableObj
-viewMatrix [$tableObj values]
+viewTable $tblObj
+viewMatrix [$tblObj values]
 wob::mainLoop
