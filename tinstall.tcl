2f210ae75daecadf61f759d04243fe981c0f4ca6
Alex Baker <ambaker1@mtu.edu>
2023-04-01 11:10:32 -0500

    Create tinstall.tcl
    Created installation file

---
+++ tinstall.tcl
@@ -0,0 +1,10 @@
+tin depend wob 0.1
+file copy [file join $src README.md] $dir
+file copy [file join $src LICENSE] $dir
+file copy [file join $src lib/tda.tcl] $dir
+file copy [file join $src lib/pkgIndex.pdf] $dir
+file copy [file join $src lib/ndlist.tcl] $dir
+file copy [file join $src lib/tbl.tcl] $dir
+file copy [file join $src lib/io.tcl] $dir
+file copy [file join $src lib/vis.tcl] $dir
+tin provide tda 0.1.1
