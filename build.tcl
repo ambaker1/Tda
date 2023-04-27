e71954f6904581a6d8d888ba673aad435e90fc87
Alex Baker <ambaker1@mtu.edu>
2023-04-26 20:03:08 -0500

    Updated docs and version number

--- build.tcl
+++ build.tcl
@@ -5,11 +5,11 @@ tin import wob; # For mainLoop
 
 # Define build configuration
 set config ""
-dict set config VERSION 0.1.1
+dict set config VERSION 0.1.2
 dict set config VERSION_NDLIST 0.1.0
 dict set config VERSION_TBL 0.1.0
 dict set config VERSION_IO 0.1.0
-dict set config VERSION_VIS 0.1.0
+dict set config VERSION_VIS 0.1.1
 # Bake the source files
 foreach inFile [glob -directory src *.tin] {
     set outFile [file join build [file rootname [file tail $inFile]].tcl]
--- install.tcl
+++ install.tcl
@@ -1,4 +1,4 @@
 package require tin 0.4.2
 tin depend wob 0.1
-set dir [tin mkdir -force tda 0.1.1]
+set dir [tin mkdir -force tda 0.1.2]
 file copy pkgIndex.tcl ndlist.tcl tbl.tcl io.tcl vis.tcl README.md LICENSE $dir
